eb1b3c1579edf51a782fb38cd2a0d24c0d286a99
Manfreack <fim.97@hotmail.com>
2019-11-11 00:55:49 -0300

    Piso (Estilo 1) randomizado
    Renombrados todos los estilos de piso para su fácil ubicación. Randomizados los del estilo 1 para tener grietas "aleatorias".

---
+++ Assets/Objects and Particles/Castle Props + Textures/Tiles/New Floors/FloorSwitcher.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class FloorSwitcher : MonoBehaviour
+{
+
+    public List<GameObject> GameObjectsMeshesToChange;
+    public List<Mesh> meshVariantes;
+    public Material newMaterial;
+    public bool randomizeMeshes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (randomizeMeshes)
+        {
+            Randomize();
+            randomizeMeshes = false;
+        }   
+    }
+
+    void Randomize()
+    {
+        foreach (var mesh in GameObjectsMeshesToChange)
+        {
+            Mesh auxMesh = meshVariantes[Random.Range(0, meshVariantes.Count)];
+
+            mesh.GetComponent<MeshFilter>().mesh = auxMesh;
+            mesh.GetComponent<Renderer>().material = newMaterial;
+        }
+    }
+}
