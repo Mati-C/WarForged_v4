dbf22edbb450ac9f70494600e1f8808a47555cd4
NahuelAlcazar <nahuel.alcazarvelasco@davinci.edu.ar>
2020-05-28 22:58:50 -0300

    Actualization Mesh renderer Camera occluder

---
+++ Assets/Scripts/OccludeMeshRendererPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OccludeMeshRendererPlayer : MonoBehaviour
+{
+    public List<SkinnedMeshRenderer> _SkinMeshRenderPlayer = new List<SkinnedMeshRenderer>();
+
+    public MeshRenderer _meshRenderPlayer;
+
+    private void OnTriggerEnter(Collider BoxCollisionWhithPlayer)
+    {
+        if (BoxCollisionWhithPlayer.gameObject.CompareTag("Player"))
+        {
+            foreach (var item in _SkinMeshRenderPlayer)
+            {
+                item.enabled = false;
+            }
+            _meshRenderPlayer.enabled = false;
+        }
+    }
+    private void OnTriggerExit(Collider BoxCollisionWhithPlayer)
+    {
+        if (BoxCollisionWhithPlayer.gameObject.CompareTag("Player"))
+        {
+            foreach (var item in _SkinMeshRenderPlayer)
+            {
+                item.enabled = true;
+            }
+            _meshRenderPlayer.enabled = true;
+        }
+    }
+}
